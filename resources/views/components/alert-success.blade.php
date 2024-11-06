265a986f46d609b839eb64a90decf254c50308d9
Eric Byrne <n00231238@iadt.ie>
2024-11-06 23:01:41 +0000

    finished lesson 7 but Restaurant created message nof fully working

---
+++ resources/views/components/alert-success.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
+    @if(session('success'))
+    <div class="mb-4 px-4 py-2 bg-green-100 border border-green-500 text-green-700 rounded-md">
+        {{$slot}}
+    </div>
+    @endif
+</div>
