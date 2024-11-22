b15ee99b55d07cf5b8abdd121d2777319c9d3f3b
= <=>
2024-11-22 12:29:54 +0000

    Started Lesson 10

---
+++ database/migrations/2024_11_22_103743_create_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            //Cascade means if the restaurant is deleted the reviews will also be deleted
+            $table->foreignId('restaurant_id')->constrained()->onDelete('cascade');
+            //Cascade means if the user is deleted so are thier reviews
+            $table->foreignId('user_id')
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
